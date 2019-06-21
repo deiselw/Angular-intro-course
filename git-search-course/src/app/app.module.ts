179b143a99533818b3733a827e4aa0e2be0b3fdf
deiselw <deiselw@gmail.com>
2019-06-21 08:30:20 -0400

    Uploading project related to Angular fundamentals course.

---
+++ git-search-course/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GitSearchService } from './git-search.service';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', 
+    component: HomePageComponent 
+  },
+  { path: 'search',      
+    redirectTo: '/search/angular', 
+    pathMatch: 'full'
+  }, 
+  {
+    path: 'search/:query',
+    component: GitSearchComponent,
+    data: { title: 'Git Search' }
+  },
+  {
+    path: 'search/:query/page/:page',
+    component: GitSearchComponent,
+    data: { title: 'Git Search' }
+  },
+  { path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  providers: [GitSearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
